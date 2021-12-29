2135b94f58d0479312d2c74859ba23997e09ddc9
Agustin Fernandez <aaferna@outlook.com.ar>
2021-12-28 21:10:28 -0300

    1.1.0

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const   { awsUpload, awsDownload } = require('./modules/core'),
-        { loggering } = require('loggering')
+        { loggering } = require('loggering'),
         chokidar = require('chokidar'),
         path = require('path'),
         arrStore = [
@@ -13,25 +13,30 @@ const   { awsUpload, awsDownload } = require('./modules/core'),
             "OUTPOSTS",
             "GLACIER_IR"
         ]
-
-global.deployPath
-
-if(process.argv[2] === "dev"){
+global.devflg = false
+if(process.argv[3] === "dev"){
+    devflg = true
     deployPath = path.dirname(__filename);
     logdir =  path.join(deployPath, "/log/")
     config = path.join(deployPath, "/config.json")
     dirtowatch = path.join(deployPath, "/watchdir.json")
-
+    CONFIGFile = require(config)
 } else {
-    deployPath = path.dirname(process.execPath);
-    logdir =  path.join(deployPath, "/log/")
-    config = path.join(deployPath, "/config.json")
+    if(process.argv[2] === "verbose") devflg = true ;
+    deployPath = path.dirname(process.execPath),
+    logdir =  path.join(deployPath, "/log/"),
+    config = path.join(deployPath, "/config.json"),
     dirtowatch = path.join(deployPath, "/watchdir.json")
+    CONFIGFile = require(config)
 }
 
-global.CONFIGFile = require(config)
+loggering('system', {msg: "Sistema Inicializado"} , logdir) ;
+if (devflg) console.log(`Sistema Inicializado`);
 
 require(dirtowatch).map(a=>{
+    
+    loggering('system', {msg: `Monitoreando el directorio ${a.dir}`} , logdir) ;
+    if (devflg) console.log(`Monitoreando el directorio ${a.dir}`);
 
     const   watcher = chokidar.watch(a.dir, { ignored: /(^|[\/\\])\../, persistent: true })
     let     storetype 
@@ -40,16 +45,20 @@ require(dirtowatch).map(a=>{
     else storetype = "STANDARD" ;
                 
     watcher.on('add', path => {
+        if (devflg) console.log(`Archivo ${path} identificado para subir`);
         
         loggering('upload', `Archivo ${path} identificado para subir`, logdir)
-            awsUpload (path, a.tagging, storetype).then(data=>{
-                loggering('upload', data, logdir)
-            }).catch(err => {
-                loggering('error', JSON.stringify(err), logdir)
-            })
+        awsUpload (path, a.tagging, storetype).then(data=>{
+            if (devflg) console.log(data);
+            loggering('upload', data, logdir)
+        }).catch(err => {
+            if (devflg && err.length !== 0) console.log(`Archivo ${path} borrado del directorio`);
+            if(err.length !== 0) loggering('system', JSON.stringify(err), logdir) ;
+        })
+
     })
     .on('unlink', path => {
-        loggering('upload', `Archivo ${path} borrado del directorio`, logdir)
-
+        // if (devflg) console.log(`Archivo ${path} borrado del directorio`);
+        // loggering('upload', `Archivo ${path} borrado del directorio`, logdir)
     });
 })
--- modules/core.js
+++ modules/core.js
@@ -1,96 +1,126 @@
-const   { unlinkSync, createReadStream, writeFileSync, statSync } = require('fs'),
+const   { unlinkSync, createReadStream, stat } = require('fs'),
         AWS = require('aws-sdk'),
-        { basename } = require("path");
+        { basename } = require("path"),
+        { loggering } = require('loggering');
 
 
 // -- Vigilante de Archivos
 exports.ControllerFile = (file) => {
+  let timer
 
     return new Promise((resolve, reject) => {
+      try {
+        let sizeInit = 0
+        timer = setInterval(() =>{
+            stat(file, (err, stats) =>{
+              
+              if(err){
+                loggering('system', {file: file, msg: err}, logdir)
+                timerClose() 
+                reject(false) 
+              }
+
+              else {
+                if (sizeInit === 0){ sizeInit = stats.mtimeMs } 
+                else if (sizeInit === stats.mtimeMs){
+                  timerClose()
+                  resolve(true)
+                } else { sizeInit = stats.mtimeMs }
+              }
+
+            })
+        }, 5000);
+      } catch(err) {
+        timerClose() 
+        if (devflg) console.log(`error en controllerfile ${err}`);
+        loggering('system', err, logdir)
+        reject(false) 
+      }
       
-      let sizeInit = 0,
-          timer = setInterval(() =>{
-          
-            try{
-  
-              let stats = statSync(file)
-  
-              if (sizeInit === 0){ sizeInit = stats.mtimeMs } 
-              else if (sizeInit === stats.mtimeMs){
-                timerClose()
-                resolve(true)
-              } else { sizeInit = stats.mtimeMs }
-            } catch(err) {
-              reject(false) 
-              timerClose() 
-            }
-            
-          }, 5000);
-  
-  
       const timerClose = () =>{
         clearInterval(timer);
       }
-      
+
+    }).then(null, () => { 
+      if (devflg) console.log({err: `el proceso de control se detuvo abruptamente para el archivo ${file}`});
+      loggering('system', {err: `el proceso de control se detuvo abruptamente para el archivo ${file}`}, logdir) 
     });
   }
 
-
   // -- Upload de File
 exports.awsUpload = (file, tagging = "", storageType) =>{
-  let namewochst = basename(file).replace(/[&\/\\#,áóúéí+()$~%.'":*?<>{}]/g, '_')
+  
+  let namewochst = basename(file).replace(/[&\/\\#,áóúéí+()$~%'":*?<>{}]/g, '_')
 
     return new Promise((resolve, reject) => {
 
-      this.ControllerFile(file).then( () => {
-       
-        let s3 = new AWS.S3({
-                accessKeyId: CONFIGFile.awsKey,
-                secretAccessKey: CONFIGFile.awsAccessKey
-            })
+      this.ControllerFile(file).then( (data) => {
 
-            s3res = s3.upload ({
-                Bucket: CONFIGFile.awsBucket,
-                Key: namewochst,
-                StorageClass: storageType,
-                Body: createReadStream(file),
-                Tagging: "Filename="+basename(namewochst)+"&"+tagging,
-                ContentType: 'application/octet-stream'
+        if (data){
 
-            }, (err, data) =>{
+          let s3 = new AWS.S3({
+              accessKeyId: CONFIGFile.awsKey,
+              secretAccessKey: CONFIGFile.awsAccessKey
+          })
 
-              if(err){
-                reject({
-                    status: false,
-                    filename: {
-                      original:basename(file),
-                      path: file
-                    },
-                    data : err
-                })
-              }
-              else {
-                unlinkSync(file)
-                resolve( {
+          s3res = s3.upload ({
+              Bucket: CONFIGFile.awsBucket,
+              Key: namewochst,
+              StorageClass: storageType,
+              Body: createReadStream(file),
+              Tagging: "Filename="+basename(namewochst)+"&"+tagging,
+              ContentType: 'application/octet-stream'
+
+          }, (err, data) =>{
+
+            if(err){
+              if (devflg) console.log({err: `el proceso de subida de archivos a S3 se detuvo abruptamente para el archivo ${file}`, msg: err});
+              loggering('system', {err: `el proceso de subida de archivos a S3 se detuvo abruptamente para el archivo ${file}`, msg: err}), logdir
+              reject()
+            }
+            else {
+              if (devflg) console.log({
                     status: true,
                     filename: {
                       original: basename(file),
                       inbucket: namewochst
                     },
                     s3response: {
-                      ServerSideEncryption: data.ServerSideEncryption,
+                      ServerSideEncryption: data.ServerSideEncryption ? data.ServerSideEncryption : "No se encuentra encriptado del lado de S3",
                       Location: data.Location,
                       Bucket: data.Bucket
                     }
-                    
-                })
-              }
-          
-            })
+                });
 
+                try {
+                  unlinkSync(file)
+                } catch (error) {
+                  if (devflg) console.log({err: `existe un error al eliminar el archivo ${file}`});
+                  loggering('system', {err: `existe un error al eliminar el archivo ${file}`}, logdir) 
+                }
+                resolve( {
+                  status: true,
+                  filename: {
+                    original: basename(file),
+                    inbucket: namewochst
+                  },
+                  s3response: {
+                    ServerSideEncryption: data.ServerSideEncryption ? data.ServerSideEncryption : "No se encuentra encriptado del lado de S3",
+                    Location: data.Location,
+                    Bucket: data.Bucket
+                  }
+              })
+            }
+        
+          })
+
+        }
           
       })
   
+    }).then(null, () => { 
+      if (devflg) console.log({err: `el proceso de control se detuvo abruptamente para el archivo ${file}`});
+      loggering('system', {err: `el proceso de control se detuvo abruptamente para el archivo ${file}`}, logdir) 
     });
 
 }
